e76fa73f9fce7740a5644580f8819958a1423bdb
Avnish Kumar Kashyap <37414321+kashyap02@users.noreply.github.com>
2018-09-02 00:35:30 +0530

    Update FamilyActivity.java

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -1,31 +1,13 @@
 package com.example.android.miwok;
-
-
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-
-
 public class FamilyActivity extends AppCompatActivity {
-
-
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_category);
-
         getSupportFragmentManager().beginTransaction()
-
                 .replace(R.id.container, new FamilyFragment())
-
                 .commit();
-
     }
-
 }
